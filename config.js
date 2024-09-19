60d3cc38b2388e441c8f5ef61b8b6b9e73efb246
Charlie <144838913+shadethestarseeker@users.noreply.github.com>
2024-09-19 20:15:28 +0000

    stuff

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
-    "gointerstellar.app": "",
+    "xhunterbase.app": "",
     localhost: "",
   },
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    ShadeTheStarseeker: "stabyourself",
   },
 };
 
