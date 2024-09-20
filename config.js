801f02de6c2f8f9e5502f6c22f0f5c8b1caca745
Charlie <144838913+shadethestarseeker@users.noreply.github.com>
2024-09-20 17:58:17 +0000

    added new login

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   blocked: {
     // Normal users can ignore this option. This is for the owner of the website.
-    "xhunterbase.app": "",
+    "interstellar.app": "",
     localhost: "",
   },
   users: {
     // You can add multiple users by doing username: 'password'.
-    ShadeTheStarseeker: "B00t9str@p1337",
+    ShadeTheStarseeker: "B00t9str@p1337", regularuser: "password"
   },
 };
 
